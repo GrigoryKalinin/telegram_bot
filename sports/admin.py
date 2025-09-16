de03a11c023916832952abd02b3b876e68fa6dda
GrigoryKalinin <GrigoryKalinin@mail.ru>
2025-09-16 14:43:16 +0500

    - настоена админка для заполнения форм тренера, спорта, района и места - Сделаны миграции, создан суперпользователь

--- sports/admin.py
+++ sports/admin.py
@@ -1,30 +1,32 @@
 from django.contrib import admin
 from .models import *
 
+
 @admin.register(Sport)
 class SportAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'available', 'is_paid', 'get_coaches', 'get_places', 'get_districts']
+    list_display = ['name', 'description', 'available']
     search_fields = ['name']
-    list_editable = ['available', 'is_paid']
+    list_editable = ['available']
     list_filter = ['name']
-    
-    def get_coaches(self, obj):
-        return ", ".join([str(coach) for coach in obj.coach.all()])
-    get_coaches.short_description = 'Тренеры'
-    
-    def get_places(self, obj):
-        return ", ".join([str(place) for place in obj.place.all()])
-    get_places.short_description = 'Места'
-    
-    def get_districts(self, obj):
-        return ", ".join([str(district) for district in obj.district.all()])
-    get_districts.short_description = 'Районы'
 
 
 @admin.register(Coach)
 class CoachAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'middle_name', 'description', 'available', 'phone']
-    search_fields = ['first_name', 'last_name', 'middle_name']
+    search_fields = ['last_name']
     list_editable = ['available']
-    list_filter = ['available', 'phone']
+    list_filter = ['last_name']
 
+
+@admin.register(District)
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ['name', 'available']
+    list_editable = ['available']
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'available', 'district']
+    search_fields = ['name']
+    list_editable = ['available']
+    list_filter = ['district']
